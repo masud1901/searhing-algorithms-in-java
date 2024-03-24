af8baa8e98ede25485e65afec8f5d1c52fb6cf84
masud1901 <akmolmasud5@gmail.com>
2024-03-25 00:43:32 +0600

    LeetCodeSolve1295 solution added

--- src/LeetCodeSolve1295.java
+++ src/LeetCodeSolve1295.java
@@ -1,18 +1,38 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class LeetCodeSolve1295 {
-    public static void main(String[] args) {
 
 //        Find Numbers with Even Number of Digits
 //        Input: nums = [12,345,2,6,7896]
 //        Output: 2
 
-        List<Integer> numsList = Arrays.asList(12, 345, 2, 6, 7896);
-        ArrayList<Integer> nums = new ArrayList<>(numsList);
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCodeSolve1295 {
+    public static void main(String[] args) {
+
+        int[] nums = {12, 345, 2, 6, 7896};
 
 
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            numbers.add(nums[i]);
+        }
+
+        List<String> numsAsStrings = new ArrayList<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            numsAsStrings.add(numbers.get(i).toString());
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < numsAsStrings.size(); i++) {
+            if (numsAsStrings.get(i).length() % 2 == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
 
 
     }
